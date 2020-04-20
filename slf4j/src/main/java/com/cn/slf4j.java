dfdee426b30a704208afc50d06813646a1cc692d
2271738697 <2271738697>
2020-04-20 16:13:08 +0800

    学习SpringBoot

---
+++ slf4j/src/main/java/com/cn/slf4j.java
@@ -0,0 +1,15 @@
+package com.cn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhoudb
+ * @date 2019/12/23 16:59
+ */
+@SpringBootApplication
+public class slf4j {
+    public static void main(String[] args) {
+        SpringApplication.run(slf4j.class,args);
+    }
+}
